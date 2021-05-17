80952284d995fb8666295bcd09985f67cfe86055
Colin Mill <colin.mill@student.fh-kiel.de>
2021-05-17 11:14:05 +0200

    Add project files.

---
+++ FM8020-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FM8020_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
