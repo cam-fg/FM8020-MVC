09797343ca06ec94b367e9d306f0fa6470a1cb1c
Vera <vera.brennecke@stud.hs-hannover.de>
2021-05-28 16:57:10 +0200

    add filter and sorting

--- FM8020-MVC/Controllers/HomeController.cs
+++ FM8020-MVC/Controllers/HomeController.cs
@@ -27,9 +27,62 @@ namespace FM8020_MVC.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Dashboard()
+        [Route("Home/Dashboard/{filterString?}/{sortOrder?}")]
+        public async Task<IActionResult> Dashboard(string filterString, string sortOrder)
         {
-            return View(await _context.Defects.Include(m => m.Room).ThenInclude(m => m.Facility).ToListAsync());
+/*            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["RoomSortParm"] = sortOrder == "Room" ? "room_desc" : "Room";*/
+            ViewData["CurrentFilter"] = filterString;
+            var defects = from d in _context.Defects
+                           select d;
+            DefectViewModel defectVM = new DefectViewModel(
+                defects.Count(),
+                defects.Where(d => d.Done == false).Count(),
+                defects.Where(d => d.Timestamp.Date == DateTime.Today).Count(),
+                defects.Where(d => d.Done).Count()
+                );
+             
+
+            if (!String.IsNullOrEmpty(filterString))
+            {
+                switch (filterString)
+                {
+                    case "Open":
+                        defects = defects.Where(d => d.Done == false);
+                        break;
+                    case "New":
+                        defects = defects.Where(d => d.Timestamp.Date == DateTime.Today);
+                        break;
+                    case "Done":
+                        defects = defects.Where(d => d.Done);
+                        break;
+                    case "All":
+                        break;
+                    default:
+                        defects = defects.Where(d => d.Done == false);
+                        break;
+                }
+            }
+
+            switch (sortOrder)
+            {
+                case "Date":
+                    defects = defects.OrderBy(d => d.Timestamp);
+                    break;
+                case "date_desc":
+                    defects = defects.OrderByDescending(d => d.Timestamp);
+                    break;
+                case "Room":
+                    defects = defects.OrderBy(d => d.Room.RoomNumber);
+                    break;
+                default:
+                    defects = defects.OrderBy(d => d.Timestamp);
+                    break;
+            }
+
+            defectVM.FilteredDefects = await defects.Include(m => m.Room).ThenInclude(m => m.Facility).ToListAsync();
+            return View(defectVM);           
+            //return View(await defects.Include(m => m.Room).ThenInclude(m => m.Facility).ToListAsync());
         }
 
         public IActionResult Privacy()
